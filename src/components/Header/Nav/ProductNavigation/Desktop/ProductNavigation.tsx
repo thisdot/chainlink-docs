4285393319c639e47c6c49b664065341dca87766
Mark Shenouda <mark.s.shenouda@gmail.com>
2024-07-03 07:08:24 +0300

    feat: Header

--- src/components/Header/Nav/ProductNavigation/Desktop/ProductNavigation.tsx
+++ src/components/Header/Nav/ProductNavigation/Desktop/ProductNavigation.tsx
@@ -9,6 +9,7 @@ import styles from "./productNavigation.module.css"
 import { SubProductContent } from "./SubProductContent"
 import { Trigger } from "./Trigger"
 import externalArrow from "../../../../../assets/icons/external-arrow.svg"
+import { CaretIcon } from "../CaretIcon"
 
 type Props = {
   path: string
@@ -25,7 +26,7 @@ const Viewport = extendRadixComponent(NavigationMenu.Viewport)
 const RadixTrigger = extendRadixComponent(NavigationMenu.Trigger)
 const RadixContent = extendRadixComponent(NavigationMenu.Content)
 
-export const ProductNavigation = ({ path, setNavMenuOpen, productsNav, subProductsNav }: Props) => {
+export const ProductNavigation = ({ path, setNavMenuOpen, subProductsNav }: Props) => {
   const productMenuRef = React.useRef<HTMLButtonElement>(null)
   const productMenuDataset = productMenuRef.current?.dataset ?? {}
   const productMenuOpen = React.useMemo(() => productMenuDataset.state === "open", [productMenuDataset.state])
@@ -45,30 +46,44 @@ export const ProductNavigation = ({ path, setNavMenuOpen, productsNav, subProduc
       <Root className={clsx(styles.root, styles.alignLeft)}>
         <List className={styles.list}>
           <Item>
-            <RadixTrigger className="nav-subproduct" ref={subProductMenuRef}>
-              <Trigger icon={icon} label={label} />
+            <RadixTrigger>
+              <Trigger label="Resources" className={styles.trigger} />
             </RadixTrigger>
-            {(subProductTrigger || label === "Resources") && subProductsNav && (
-              <RadixContent className={styles.content}>
-                <SubProductContent subProductsNav={subProductsNav} />
-              </RadixContent>
-            )}
-          </Item>
-          <Indicator className={styles.indicator}>
-            <div className={styles.arrow} />
-          </Indicator>
-        </List>
 
-        <div className={styles.viewportPosition}>
-          <Viewport className={styles.navigationViewport} />
-        </div>
-        <Divider className={styles.divider} />
-        <NavigationMenu.Link className={styles.button} href="https://dev.chain.link" target="_blank">
-          Developer Hub
-          <div>
-            <img src={externalArrow.src}></img>
-          </div>
-        </NavigationMenu.Link>
+            <NavigationMenu.Content className={styles.resourcesMenuContent}>
+              <div className={styles.resourcesMenuContentMain}>
+                <div className={styles.resourcesMenuContentRow}>Row</div>
+                <div className={styles.resourcesMenuContentRow}>Row</div>
+                <div className={styles.resourcesMenuContentRow}>Row</div>
+                <div className={styles.resourcesMenuContentRow}>Row</div>
+              </div>
+              <div className={styles.resourcesMenuContentFeatured}>
+                <h2>Featured</h2>
+                <img src="/images/quick-start.png" alt="" className={styles.featuredImage} />
+              </div>
+            </NavigationMenu.Content>
+          </Item>
+          <Item>
+            <NavigationMenu.Link className={styles.navLink} href="https://dev.chain.link" target="_blank">
+              Docs
+            </NavigationMenu.Link>
+          </Item>
+          <Item>
+            <NavigationMenu.Link className={styles.navLink} href="https://dev.chain.link" target="_blank">
+              Demos
+            </NavigationMenu.Link>
+          </Item>
+          <Item>
+            <NavigationMenu.Link className={styles.navLink} href="https://dev.chain.link" target="_blank">
+              Tools
+            </NavigationMenu.Link>
+          </Item>
+          <Item>
+            <NavigationMenu.Link className={styles.navLink} href="https://dev.chain.link" target="_blank">
+              ChainLog
+            </NavigationMenu.Link>
+          </Item>
+        </List>
       </Root>
     </>
   )

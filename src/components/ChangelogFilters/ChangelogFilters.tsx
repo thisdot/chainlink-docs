4c220ca0b652b2933c7bf47fc2e1a8d6648c1c2a
Tyrel Chambers <tychambers3@gmail.com>
2025-11-26 15:31:39 -0500

    lots of styling

--- src/components/ChangelogFilters/ChangelogFilters.tsx
+++ src/components/ChangelogFilters/ChangelogFilters.tsx
@@ -1,5 +1,59 @@
+import { SvgSearch, SvgTaillessArrowDownSmall, SvgX } from "@chainlink/blocks"
 import styles from "./styles.module.css"
+import { useState } from "react"
+import { clsx } from "~/lib/clsx/clsx.ts"
+
+const SearchInput = ({ isExpanded, onClick }: { isExpanded: boolean; onClick: (value: boolean) => void }) => {
+  return (
+    <div className={clsx(styles.searchInputWrapper, isExpanded && styles.expanded)} onClick={() => onClick(true)}>
+      <SvgSearch className={styles.searchIcon} color="pill-active" />
+      <input
+        placeholder={!isExpanded ? "Search" : "Search a product, network or type"}
+        className={styles.searchInput}
+      />
+      {isExpanded && (
+        <SvgX
+          color="pill-active"
+          onClick={(e) => {
+            e.stopPropagation()
+            onClick(false)
+          }}
+          style={{
+            marginRight: "var(--space-4x)",
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+const Trigger = ({ label }: { label: string }) => {
+  return (
+    <button className={styles.btn}>
+      <span>{label}</span> <SvgTaillessArrowDownSmall color="pill-active" />
+    </button>
+  )
+}
 
 export const ChangelogFilters = () => {
-  return <div className={styles.wrapper}>hi</div>
+  const [searchExpanded, setSearchExpanded] = useState(false)
+
+  const searchClickHandler = (value: boolean) => {
+    setSearchExpanded(value)
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.expandedContent}></div>
+      <div className={styles.content}>
+        {!searchExpanded && (
+          <>
+            <Trigger label="Product" />
+            <Trigger label="Network" />
+            <Trigger label="Type" />
+          </>
+        )}
+        <SearchInput isExpanded={searchExpanded} onClick={searchClickHandler} />
+      </div>
+    </div>
+  )
 }
